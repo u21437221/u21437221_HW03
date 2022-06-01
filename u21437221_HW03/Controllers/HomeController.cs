d4041bb3768daa98bb444cf47e10dcdaea3736e7
u21437221 <u21437221@tuks.co.za>
2022-06-01 19:52:42 +0200

    Finishing the home page, still need to save the files in the appropriate folders.

--- u21437221_HW03/Controllers/HomeController.cs
+++ u21437221_HW03/Controllers/HomeController.cs
@@ -3,15 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
 
 namespace u21437221_HW03.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
+
+        //[HttpPost]
+        //public ActionResult Index(HttpPostedFileBase file)
+        //{
+        //    try
+        //    {
+        //        if (file.ContentLength > 0)
+        //        {
+        //            string _FileName = Path.GetFileName(file.FileName);
+        //            string _path = Path.Combine(Server.MapPath("~/Media"), _FileName);
+        //            file.SaveAs(_path);
+        //        }
+        //        ViewBag.Message = "File uploaded successfully";
+        //        return View();
+        //    }
+        //    catch
+        //    {
+        //        ViewBag.Message = "File upload failed";
+        //        return View();
+        //    }
+        //}
 
         public ActionResult About()
         {
