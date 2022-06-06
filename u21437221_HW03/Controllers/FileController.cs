ea1a7b9225bfdc98fb85db5cb0180f5d38c34c22
u21437221 <u21437221@tuks.co.za>
2022-06-06 22:44:29 +0200

    File page and controller done, remember to put headings in the table

--- u21437221_HW03/Controllers/FileController.cs
+++ u21437221_HW03/Controllers/FileController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.IO;
 
 namespace u21437221_HW03.Controllers
 {
@@ -11,7 +12,31 @@ namespace u21437221_HW03.Controllers
         // GET: File
         public ActionResult FileIndex()
         {
-            return View();
+            string[] folderFiles = Directory.GetFiles(Server.MapPath("~/Media/Documents"));
+            List<Models.FileModel> file = new List<Models.FileModel>();
+            foreach (string folderFile in folderFiles)
+            {
+                file.Add(new Models.FileModel { FileName = Path.GetFileName(folderFile) });
+            }
+            return View(file);
+        }
+
+        public FileResult DownloadFile(string Name)
+        {
+            string fileDestination = Server.MapPath("~/Media/Documents/") + Name;
+            byte[] array = System.IO.File.ReadAllBytes(fileDestination); 
+            return File(array, "application/octet-stream", Name);
+        }
+
+        public ActionResult DeleteFile(string Name)
+        {
+            string fullFile = Request.MapPath("~/Media/Documents/" + Name);
+            if (System.IO.File.Exists(fullFile))
+            {
+                System.IO.File.Delete(fullFile);
+                ViewBag.deleteSuccess = "true";
+            }
+            return RedirectToAction("FileIndex");
         }
     }
 }
